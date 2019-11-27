1c503d63c8ed5d14acaeb6a461d26e94eb4c3e00
Christian Pardillo Laursen <ninja1511@gmail.com>
2019-11-27 09:12:45 +0000

    Added PMCS

--- Assessment/q1.py
+++ Assessment/q1.py
@@ -29,31 +29,26 @@ def main():
         for line in data_lines:
             for n, i in enumerate(line.strip().split(',')):
                 data[headers[n]].append(int(i))
-        print(probMLE(data['Y'], 0))
-        print(probMLECond(data['X1'], data['Y'], 1))
-        print(probMLECond(data['X1'], data['Y'], 0))
     formatAnswer(data)
 
 def formatAnswer(data):
-    print(f'''
-MLE:
-P(Y=0) = {probMLE(data['Y'], 0)}
-P(X1=0|Y=0) = {probMLECond(data['X1'], data['Y'], 0)}
-P(X1=0|Y=1) = {probMLECond(data['X1'], data['Y'], 1)}
-P(X2=0|Y=0) = {probMLECond(data['X2'], data['Y'], 0)}
-P(X2=0|Y=1) = {probMLECond(data['X2'], data['Y'], 1)}
-P(X3=0|Y=0) = {probMLECond(data['X3'], data['Y'], 0)}
-P(X3=0|Y=1) = {probMLECond(data['X3'], data['Y'], 1)}
+    print(f'''MLE:
+P(Y=0) = {round(probMLE(data['Y'], 0), 4)}
+P(X1=0|Y=0) = {round(probMLECond(data['X1'], data['Y'], 0), 4)}
+P(X1=0|Y=1) = {round(probMLECond(data['X1'], data['Y'], 1), 4)}
+P(X2=0|Y=0) = {round(probMLECond(data['X2'], data['Y'], 0), 4)}
+P(X2=0|Y=1) = {round(probMLECond(data['X2'], data['Y'], 1), 4)}
+P(X3=0|Y=0) = {round(probMLECond(data['X3'], data['Y'], 0), 4)}
+P(X3=0|Y=1) = {round(probMLECond(data['X3'], data['Y'], 1), 4)}
 
-Bayesian
-P(Y=0) = {probBayes(data['Y'], 0)}
-P(X1=0|Y=0) = {probBayesCond(data['X1'], data['Y'], 0)}
-P(X1=0|Y=1) = {probBayesCond(data['X1'], data['Y'], 1)}
-P(X2=0|Y=0) = {probBayesCond(data['X2'], data['Y'], 0)}
-P(X2=0|Y=1) = {probBayesCond(data['X2'], data['Y'], 1)}
-P(X3=0|Y=0) = {probBayesCond(data['X3'], data['Y'], 0)}
-P(X3=0|Y=1) = {probBayesCond(data['X3'], data['Y'], 1)}
-''')
+Bayesian:
+P(Y=0) = {round(probBayes(data['Y'], 0), 4)}
+P(X1=0|Y=0) = {round(probBayesCond(data['X1'], data['Y'], 0), 4)}
+P(X1=0|Y=1) = {round(probBayesCond(data['X1'], data['Y'], 1), 4)}
+P(X2=0|Y=0) = {round(probBayesCond(data['X2'], data['Y'], 0), 4)}
+P(X2=0|Y=1) = {round(probBayesCond(data['X2'], data['Y'], 1), 4)}
+P(X3=0|Y=0) = {round(probBayesCond(data['X3'], data['Y'], 0), 4)}
+P(X3=0|Y=1) = {round(probBayesCond(data['X3'], data['Y'], 1), 4)}''')
 
 if __name__ == "__main__":
     main()
